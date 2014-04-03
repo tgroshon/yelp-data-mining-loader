c9cd340a558d8420e609a5daff6dd537bb5d1e00
Thomas Groshong <thomas.groshong@gmail.com>
2014-04-03 02:11:26 -0600

    Initial Commit

---
+++ json_to_mysql.py
@@ -0,0 +1,122 @@
+from peewee import OperationalError
+from models import Business
+from models import Review
+from models import User
+from models import Checkin
+from models import Neighborhood
+from models import Category
+import json
+import decimal
+from datetime import datetime
+
+def iterate_file(jsonfile, shortcircuit=True):
+    i = 0
+    jsonfilename = "json/yelp_training_set_%s.json" % jsonfile
+    with open(jsonfilename) as jfile:
+        for line in jfile.readlines():
+            i += 1
+            yield json.loads(line)
+            if i % 500 == 0:
+                print("Status >>> %s: %d" % (jsonfilename, i))
+            if shortcircuit and i == 10:
+                raise StopIteration()
+    
+    
+def save_businesses():
+    for bdata in iterate_file("business", shortcircuit=False):
+        save_categories(bdata['business_id'], bdata['categories'])
+        save_neighborhoods(bdata['business_id'], bdata['neighborhoods'])
+        # business = Business()
+        # business.business_id = bdata['business_id']
+        # business.name = bdata['name']
+        # business.full_address = bdata['full_address']
+        # business.city = bdata['city']
+        # business.state = bdata['state']
+        # business.latitude = bdata['latitude']
+        # business.longitude = bdata['longitude']
+        # business.stars = decimal.Decimal(bdata.get('stars', 0))
+        # business.review_count = int(bdata['review_count'])
+        # business.is_open = True if bdata['open'] == "True" else False
+        # business.save()
+
+def save_categories(business_id, cat_jarray):
+    for name in cat_jarray:
+        category = Category()
+        category.business_id = business_id
+        category.category_name = name
+        category.save()
+
+
+def save_neighborhoods(business_id, hood_jarray):
+    for hood in hood_jarray:
+        neighborhood = Neighborhood()
+        neighborhood.business_id = business_id
+        neighborhood.neighborhood_name = hood
+        neighborhood.save()
+
+def save_reviews():
+    for rdata in iterate_file("review", shortcircuit=False):
+        rev = Review()
+        rev.business_id = rdata['business_id']
+        rev.user_id = rdata['user_id']
+        rev.stars = int(rdata.get('stars', 0))
+        rev.text = rdata['text']
+        rev.date = datetime.strptime(rdata['date'], "%Y-%m-%d")
+        rev.useful_votes = int(rdata['votes']['useful'])
+        rev.funny_votes = int(rdata['votes']['funny'])
+        rev.cool_votes = int(rdata['votes']['cool'])
+        rev.save()
+
+def save_users():
+    for udata in iterate_file("user", shortcircuit=False):
+        user = User()
+        user.user_id = udata['user_id']
+        user.name = udata['name']
+        user.review_count = int(udata['review_count'])
+        user.average_stars = decimal.Decimal(udata.get('average_stars', 0))
+        user.useful_votes = int(udata['votes']['useful'])
+        user.funny_votes = int(udata['votes']['funny'])
+        user.cool_votes = int(udata['votes']['cool'])
+        user.save()
+
+def save_checkins():
+    for cdata in iterate_file("checkin", shortcircuit=False):
+        checkin = Checkin()
+        checkin.business_id = cdata['business_id']
+        for day in range(7):
+            for hour in range(24):
+                number = int(cdata['checkin_info'].get("%s-%s" % (hour, day), 0))
+                if day is 0:
+                    checkin.sunday_count += number
+                elif day is 1:
+                    checkin.monday_count += number
+                elif day is 2:
+                    checkin.tuesday_count += number
+                elif day is 3:
+                    checkin.wednesday_count += number
+                elif day is 4:
+                    checkin.thursday_count += number
+                elif day is 5:
+                    checkin.friday_count += number
+                elif day is 6:
+                    checkin.saturday_count += number
+        checkin.save()
+
+def reset_database():
+    try:
+        # Nuke tables
+        Business.drop_table()
+        Review.drop_table()
+        User.drop_table()
+        Checkin.drop_table()
+
+        # Create clean tables
+        Business.create_table()
+        Review.create_table()
+        User.create_table()
+        Checkin.create_table()
+    except OperationalError:
+        pass
+    
+if __name__ == "__main__":
+    save_businesses()
---
+++ models.py
@@ -0,0 +1,138 @@
+import peewee
+from peewee import *
+
+
+db = MySQLDatabase('yelp_data_mining', user='USERNAME',passwd='PASSWORD')
+
+class Business(peewee.Model):
+    """
+    {
+      'type': 'business',
+      'business_id': (encrypted business id),
+      'name': (business name),
+      'neighborhoods': [(hood names)],
+      'full_address': (localized address),
+      'city': (city),
+      'state': (state),
+      'latitude': latitude,
+      'longitude': longitude,
+      'stars': (star rating, rounded to half-stars),
+      'review_count': review count,
+      'categories': [(localized category names)]
+      'open': True / False (corresponds to permanently closed, not business hours),
+    }
+    """
+    bid = peewee.PrimaryKeyField()
+    business_id = peewee.CharField()  # encrypted ID with letters, numbers, and symbols
+    name = peewee.CharField()
+    full_address = peewee.CharField()
+    city = peewee.CharField()
+    state = peewee.CharField(max_length=2)  # AZ
+    latitude = peewee.CharField()
+    longitude = peewee.CharField()
+    stars = peewee.DecimalField()  # star rating rounded to half-stars
+    review_count = peewee.BigIntegerField()
+    is_open = peewee.BooleanField()
+    # neighborhoods = None # list of hood names
+    # categories = None # list of category names
+
+    class Meta:
+        database = db
+
+class Category(peewee.Model):
+    """
+    Derived from Business.categories Field.
+    """
+    id = peewee.PrimaryKeyField()
+    business_id = peewee.CharField()
+    category_name = peewee.CharField()
+
+    class Meta:
+        database = db
+
+class Neighborhood(peewee.Model):
+    '''
+    Derived from Business.neighborhoods Field.
+    '''
+    id = peewee.PrimaryKeyField()
+    business_id = peewee.CharField()
+    neighborhood_name = peewee.CharField()
+
+    class Meta:
+        database = db
+
+class Review(peewee.Model):
+    """
+    {
+      'type': 'review',
+      'business_id': (encrypted business id),
+      'user_id': (encrypted user id),
+      'stars': (star rating),
+      'text': (review text),
+      'date': (date, formatted like '2012-03-14', %Y-%m-%d in strptime notation),
+      'votes': {'useful': (count), 'funny': (count), 'cool': (count)}
+    }
+    """
+    rid = peewee.PrimaryKeyField()
+    business_id = peewee.CharField()
+    user_id = peewee.CharField()
+    stars = peewee.IntegerField()
+    text = peewee.TextField()
+    date = peewee.DateField(formats="%Y-%m-%d")  # '2012-03-14', %Y-%m-%d in strptime notation
+    useful_votes = peewee.IntegerField()
+    funny_votes = peewee.IntegerField()
+    cool_votes = peewee.IntegerField()
+
+    class Meta:
+        database = db
+
+class User(peewee.Model):
+    """
+    {
+      'type': 'user',
+      'user_id': (encrypted user id),
+      'name': (first name),
+      'review_count': (review count),
+      'average_stars': (floating point average, like 4.31),
+      'votes': {'useful': (count), 'funny': (count), 'cool': (count)}
+    }
+    """
+    uid = peewee.PrimaryKeyField()
+    user_id = peewee.CharField()
+    name = peewee.CharField()
+    review_count = peewee.IntegerField()
+    average_stars = peewee.FloatField()
+    useful_votes = peewee.IntegerField()
+    funny_votes = peewee.IntegerField()
+    cool_votes = peewee.IntegerField()
+
+    class Meta:
+        database = db
+
+class Checkin(peewee.Model):
+    """
+    {
+      'type': 'checkin',
+      'business_id': (encrypted business id),
+      'checkin_info': {
+            '0-0': (number of checkins from 00:00 to 01:00 on all Sundays),
+            '1-0': (number of checkins from 01:00 to 02:00 on all Sundays), 
+            ... 
+            '14-4': (number of checkins from 14:00 to 15:00 on all Thursdays),
+            ...
+            '23-6': (number of checkins from 23:00 to 00:00 on all Saturdays)
+      } # if there was no checkin for an hour-day block it will not be in the dict
+    }
+    """
+    cid = peewee.PrimaryKeyField()
+    business_id = peewee.CharField()
+    sunday_count = peewee.IntegerField(default=0)
+    monday_count = peewee.IntegerField(default=0)
+    tuesday_count = peewee.IntegerField(default=0)
+    wednesday_count = peewee.IntegerField(default=0)
+    thursday_count = peewee.IntegerField(default=0)
+    friday_count = peewee.IntegerField(default=0)
+    saturday_count = peewee.IntegerField(default=0)
+
+    class Meta:
+        database = db
